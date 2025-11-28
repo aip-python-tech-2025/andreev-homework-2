0c64deb0f54dbc0c62c8cddb70a582e39f3f2ad9
Nikolay Andreev <bakasaru@list.ru>
2025-11-28 18:23:32 +0300

    feat: add inheritance example

--- encapsulation.py
+++ encapsulation.py
@@ -1,37 +1,41 @@
 class Client:
     def __init__(self, name, initial_balance):
-        self.__name = name
-        self.__balance = initial_balance
+        self._name = name
+        self._balance = initial_balance
 
     def __str__(self):
-        return f'{self.__name} with balance = {self.__balance}'
+        return f'{self._name} with balance = {self._balance}'
 
     def transfer(self, recipient, amount):
-        if self.__balance >= amount:
-            recipient.__balance += amount
-            self.__balance -= amount
+        if self._balance >= amount:
+            recipient._balance += amount
+            self._balance -= amount
 
     def deposit(self, amount):
-        self.__balance += amount
+        self._balance += amount
 
     def withdraw(self, amount):
-        if self.__balance >= amount:
-            self.__balance -= amount
+        if self._balance >= amount:
+            self._balance -= amount
+
+    def pay(self, amount):
+        if self._balance >= amount:
+            self._balance -= amount
 
     # Функция-геттер
     def get_name(self):
-        return self.__name
+        return self._name
 
     # Функция-сеттер
     def change_name(self, new_name):
         bad_words = ['дурак']
         if new_name not in bad_words:
-            self.__name = new_name
+            self._name = new_name
 
     # Геттер
     @property
     def name(self):
-        return self.__name
+        return self._name
 
     # Сеттер
     @name.setter
@@ -40,12 +44,26 @@ class Client:
 
 
 class VipClient(Client):
-    pass
+    def __init__(self, name, initial_balance):
+        super().__init__(name, initial_balance)
+        self.__bonuses = 0
+
+    def pay(self, amount):
+        if self._balance >= amount:
+            self._balance -= amount
+            self.__bonuses += int(amount * 0.15)
+
+    def __str__(self):
+        return f'{self._name} with balance = {self._balance} and bonuses = {self.__bonuses}'
 
 
-alice = Client(name='Alice', initial_balance=100)
-bob = Client(name='Bob', initial_balance=200)
-charlie = VipClient(name='Charlie', initial_balance=300)
+alice = Client(name='Alice', initial_balance=1000)
+bob = Client(name='Bob', initial_balance=2000)
+charlie = VipClient(name='Charlie', initial_balance=3000)
+
+alice.pay(100)
+bob.pay(100)
+charlie.pay(100)
 
 bob.transfer(alice, 100)
 charlie.transfer(alice, 100)
@@ -58,7 +76,6 @@ print(bob)
 print(charlie)
 
 charlie.name += ' Smith'
-# charlie.change_name(charlie.get_name() + ' Smith')
 print(charlie.name)
 
 print(alice.name)
