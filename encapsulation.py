3a21c42ff2e86eacc3af1b386aaf4f4823218a74
Nikolay Andreev <bakasaru@list.ru>
2025-11-28 18:55:40 +0300

    feat: add association example

--- encapsulation.py
+++ encapsulation.py
@@ -1,7 +1,17 @@
+class Transaction:
+    def __init__(self, amount, sender=None, recipient=None):
+        self.__sender = sender
+        self.__recipient = recipient
+        self.__amount = amount
+
+    def __repr__(self):
+        return f'{self.__sender} -> {self.__recipient} with amount = {self.__amount}'
+
 class Client:
     def __init__(self, name, initial_balance):
         self._name = name
         self._balance = initial_balance
+        self._transactions = []
 
     def __str__(self):
         return f'{self._name} with balance = {self._balance}'
@@ -10,17 +20,22 @@ class Client:
         if self._balance >= amount:
             recipient._balance += amount
             self._balance -= amount
+            self._transactions.append(Transaction(amount, self, recipient))
+            recipient._transactions.append(Transaction(amount, recipient, self))
 
     def deposit(self, amount):
         self._balance += amount
+        self._transactions.append(Transaction(amount, None, self))
 
     def withdraw(self, amount):
         if self._balance >= amount:
             self._balance -= amount
+            self._transactions.append(Transaction(amount, self, None))
 
     def pay(self, amount):
         if self._balance >= amount:
             self._balance -= amount
+            self._transactions.append(Transaction(amount, self, None))
 
     # Функция-геттер
     def get_name(self):
@@ -52,6 +67,7 @@ class VipClient(Client):
         if self._balance >= amount:
             self._balance -= amount
             self.__bonuses += int(amount * 0.15)
+            self._transactions.append(Transaction(amount, self, None))
 
     def __str__(self):
         return f'{self._name} with balance = {self._balance} and bonuses = {self.__bonuses}'
@@ -61,9 +77,13 @@ alice = Client(name='Alice', initial_balance=1000)
 bob = Client(name='Bob', initial_balance=2000)
 charlie = VipClient(name='Charlie', initial_balance=3000)
 
-alice.pay(100)
-bob.pay(100)
-charlie.pay(100)
+for client in alice, bob, charlie:
+    client.pay(100)
+    print(client)
+
+# alice.pay(100)
+# bob.pay(100)
+# charlie.pay(100)
 
 bob.transfer(alice, 100)
 charlie.transfer(alice, 100)
@@ -81,3 +101,7 @@ print(charlie.name)
 print(alice.name)
 alice.name = 'Алиса'
 print(alice.name)
+
+print(alice._transactions)
+print(bob._transactions)
+print(charlie._transactions)
